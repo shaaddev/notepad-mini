8c16118161ae1499da5036f4cf7771b49a80acfa
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-01-31 01:35:14 -0500

    locked aspect ratio

--- components/notepad.tsx
+++ components/notepad.tsx
@@ -2,10 +2,10 @@ import { Textarea } from "@headlessui/react";
 
 export function Notepad() {
   return (
-    <div className="w-4/5 h-[500px] min-h-[500px]">
+    <div className="w-4/5 h-[500px]">
       <Textarea
         name="note"
-        className="w-full h-full bg-neutral-200 text-black dark:text-white dark:bg-neutral-900 rounded-xl focus:outline-none p-4"
+        className="w-full h-full  min-h-[500px] max-h-[500px] bg-neutral-200 text-black dark:text-white dark:bg-neutral-900 rounded-xl focus:outline-none p-4"
       ></Textarea>
     </div>
   );
