52020aaa8dee2d53437af550c6d5d377ce7b9ee6
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-10-10 01:54:27 -0400

    blocked mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,19 @@
-import { Notepad } from "@/components/notepad";
-import { NotepadGuest } from "@/components/notepad-guest";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { Theme } from "@/components/theme";
-import { LoginBtn, LogoutBtn } from "@/components/auth/auth-btns";
-import { auth } from "@/lib/auth/auth";
-import { headers } from "next/headers";
+import { Notes } from "@/components/notes";
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
+export default function Page() {
   return (
-    <SidebarProvider>
-      <SidebarInset>
-        <div className="min-h-screen flex flex-col items-center justify-center bg-neutral-300 dark:bg-neutral-950 gap-6">
-          <div className="w-4/5 flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <SidebarTrigger />
-            </div>
-            <div className="text-center">
-              {session ? <LogoutBtn /> : <LoginBtn />}
-            </div>
-          </div>
-          {session ? <Notepad /> : <NotepadGuest />}
-          <Theme />
+    <>
+      <div className="lg:hidden">
+        <div className="flex flex-col items-center justify-center h-screen px-4">
+          <h1 className="text-2xl font-bold mb-5">Womp Womp</h1>
+          <p className="text-sm text-center">
+            If you are reading this, try this app on a desktop
+          </p>
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+      </div>
+      <div className="hidden lg:block">
+        <Notes />
+      </div>
+    </>
   );
 }
---
+++ components/notes.tsx
@@ -0,0 +1,36 @@
+import { Notepad } from "@/components/notepad";
+import { NotepadGuest } from "@/components/notepad-guest";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Theme } from "@/components/theme";
+import { LoginBtn, LogoutBtn } from "@/components/auth/auth-btns";
+import { auth } from "@/lib/auth/auth";
+import { headers } from "next/headers";
+
+export async function Notes() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  return (
+    <SidebarProvider>
+      <SidebarInset>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-neutral-300 dark:bg-neutral-950 gap-6">
+          <div className="w-4/5 flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <SidebarTrigger />
+            </div>
+            <div className="text-center">
+              {session ? <LogoutBtn /> : <LoginBtn />}
+            </div>
+          </div>
+          {session ? <Notepad /> : <NotepadGuest />}
+          <Theme />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
